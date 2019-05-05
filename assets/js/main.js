e44d1784fb16362c3564e8da5085646ac2514ff5
Matt <matt@mwn-design.co.uk>
2019-05-05 21:44:03 +0100

    Custom JS scripts added, see description
    The custom scripts added will allow the sidebar to become viewable in mobile view when the button is pressed (button is only viewable on small screens).
    
    I have also added in a script to change the button icon when the sidebar is open or closed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-$(function() {});
+$(function() {
+    //Change repsonsive menu button when clicked
+    $('#sidebarCollapse').click(function() {
+        $(this)
+            .find('i')
+            .toggleClass('fa-bar fa-times');
+    });
+
+    //Sidebar functions
+    $('#sidebar').mCustomScrollbar({
+        theme: 'minimal'
+    });
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
+    });
+});
